e429847f2c197f89ca8f899944f4250c80a95730
Abhilasha0602 <abhilasha2482@gmail.com>
2020-07-16 13:59:01 +0530

    coursera abhilasha

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x=matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function()(x)
+  setInverse <- function(inverse){inv <<- inverse}
+  getInverse <- function(){inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+CacheSolve <- function(x,...){
+  inv <- x$getInverse()
+  if (!is.null(inv)){
+    message ("getting catched data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+Example
+> source("cachematrix.R")
+> pmatrix <- makeCachematrix(matrix(1:16, nrow=4,ncol=4))
+Error in makeCachematrix(matrix(1:16, nrow = 4, ncol = 4)) : 
+  could not find function "makeCachematrix"
+> pmatrix <- makeCacheMatrix(matrix(1:16, nrow=4,ncol=4))
+> pmatrix$get()
+[,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> pmatrix$getInverse()
+NULL
+> pmatrix <- makeCacheMatrix(matrix(1:4, nrow=2,ncol=2))
+> pmatrix$get()
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> pmatrix$getInverse()
+NULL
+> CacheSolve(pmatrix)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> CacheSolve(pmatrix)
+getting catched data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> pmatrix$getInverse()
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
